909ae7ab1afaf8fd1860eebb085d3afcbc9f9044
unknown <you@example.com>
2024-02-27 17:55:09 +0400

    frontbackk

---
+++ frontback/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using frontback.Contexts;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace frontback.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly PronioDbContext _context;
+
+        public HomeController(PronioDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+
+
+            var sliders = _context.Sliders.ToList();
+
+            return View(sliders);
+        }
+    }
+}
